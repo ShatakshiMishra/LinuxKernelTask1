2f84fc621575099fd0d5c42cf609c628809a8206
ShatakshiMishra <73740877+ShatakshiMishra@users.noreply.github.com>
2021-07-17 09:17:47 +0530

    Create char_device.c

---
+++ char_device.c
@@ -0,0 +1,47 @@
+#include <kernel.h>
+#include <init.h>
+#include <module.h>
+#include<linux/fs.h>
+
+int driver_open (struct inode *pinode, struct file *pfile)
+{
+  printk(KERN_ALERT "OPENED");
+  return 0;
+}
+ssize_t driver_read (struct file *pfile, char __user *buff, size_t len, loff_t *offset)
+{
+   printk(KERN_ALERT "READED");
+  return 0;
+}
+ssize_t driver_write (struct file *pfile, const char __user *buff, size_t len, loff_t *offset)
+{
+   printk(KERN_ALERT "WRITTEN");
+  retun len;
+ 
+}
+int driver_close (struct inode *pinode, struct file *pfile)
+{
+   printk(KERN_ALERT "CLOSED");
+  return 0;
+}
+struct file_operations file_opts=
+{  .owner= THIS_MODULE,
+  .open= driver_open,
+  .read= driver_read,
+  .write= driver_write,
+ .release= driver_close,  
+};
+
+
+static int driver_init(void)
+{
+        register_chrdev(200,"mydriver",&file_opst);
+        return 0;
+}
+static void driver_exit(void)
+{
+        unregister_chrdev(200,"mydevice");
+
+}
+module_init(driver_init);
+module_exit(driver_exit);
